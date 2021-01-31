137637963c521744ca1136f36838726abf62ad9a
renxzen <u201415624@upc.edu.pe>
2021-01-31 18:48:55 -0500

    bin

--- source/test.cpp
+++ source/test.cpp
@@ -6,57 +6,51 @@ using namespace std;
 #define printArr(a) for(int i=0;i<sizeof(a)/sizeof(a[0]);i++) cout<<a[i]<<" "; cout<<"\n"
 
 struct Factorials {
-	long long array[20] = {1}; // factorials[0] = 1 
+	ll array[20] = {1}; // factorials[0] = 1 
+	ll bin[20][20];
+
 	Factorials() {
 		for (int i = 1; i <= 20; i++) array[i] = i * array[i-1];
+
+		bin[0][0] = 1;
+		for (int i = 1; i <= 20; i++) {
+			bin[i][0] = 1;
+			bin[0][i] = 0;
+		}
+
+		for (int i = 1; i <= 20; i++) {
+			for (int j = 1; j <= 20; j++) {
+				if (i < j) {
+					bin[i][j] = -1;
+					continue;
+				}
+
+				bin[i][j] = array[i]/(array[j]*array[i-j]);
+			}
+		}
 	}
 
 	long long factorial(int n){
 		return array[n];
 	}
 
-	int binomial(int n, int m) {
-		return array[n]/(array[m]*array[n-m]);
-	}
-};
-
-struct Punto {
-	float x, y, z;
-	Punto(float x = 0, float y = 0, float z = 0): x(x), y(y), z(z) {}
-
-	void printIt() {
-		cout << x << " " << y << " " << z << endl;
-	}
-
-	Punto& setx(float a){
-		x = a;
-		return *this;
-	}
-
-	Punto& sety(float a){
-		y = a;
-		return *this;
-	}
-
-	Punto& operator=(Punto& punto){
-		x = punto.x;
-		y = punto.y;
-		z = punto.z;
-		return *this;
+	long long binomial(int n, int m) {
+		return bin[n][m];
 	}
 
 };
-
 
 int main() {
+	cout << "okay\n";
+
 	Factorials fact;
 	cout << fact.factorial(5) << endl;
-	cout << fact.binomial(7,2) << endl;
 
-	vector<Punto> puntosControl;
-	for(int i=0; i<5; i++){
-		puntosControl.push_back(Punto());
-		puntosControl[i].setx(i+i).sety(i*i).printIt();
+	for (int i = 0; i <= 20; i++) {
+		for (int j = 0; j <= 20; j++) {
+			cout << fact.binomial(i,j) << "\t";
+		}
+		cout << endl;
 	}
 
 }
